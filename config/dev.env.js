b07675e79efb240e44f36f9c5ce75bc07d7359de
郝仕兢 <haoshijing@corp.netease.com>
2018-04-17 10:01:46 +0800

    服务器迁移

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,6 +1,6 @@
 module.exports = {
     NODE_ENV: '"development"',
     ENV_CONFIG: '"dev"',
-    BASE_API: '"http://115.159.29.17:8123/admin"',
+    BASE_API: 'http://47.75.167.230:8123/admin"',
     APP_ORIGIN: '"https://wallstreetcn.com"'
 }
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
     NODE_ENV: '"production"',
     ENV_CONFIG: '"prod"',
-    BASE_API: '"http://115.159.29.17:8123/admin/"',
+    BASE_API: '"http://47.52.135.205:8123/admin/"',
     APP_ORIGIN: '"https://wallstreetcn.com"'
 };
