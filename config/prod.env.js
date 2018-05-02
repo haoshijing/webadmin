b388a7824aa209a1635b8fa0950db77b9dde5adf
郝仕兢 <haoshijing@corp.netease.com>
2018-05-02 16:31:02 +0800

    配置修改

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
     NODE_ENV: '"production"',
     ENV_CONFIG: '"prod"',
-    BASE_API: '"http://127.0.0.1:8080/"',
+    BASE_API: '"http://123.56.219.172:8080/encrypt"',
     APP_ORIGIN: '"https://wallstreetcn.com"'
 };
